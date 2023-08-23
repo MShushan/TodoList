8406e693fe45f544173a7b18bb6e375160445762
Shushan Mirzoyan <shushanmirzoyan1@gmail.com>
2023-08-23 21:34:14 +0400

    search component has been changed

---
+++ src/components/Search.js
@@ -0,0 +1,4 @@
+const Search = () => {
+    return <input type='text' placeholder='type text for search'/>;
+  }
+   export default Search;
---
+++ src/components/TodoList.js
@@ -0,0 +1,10 @@
+const TodoList = () => {
+    return (
+      <ul>
+        <li>Learn Js</li>
+        <li>Learn React</li>
+      </ul>
+    );
+    
+  }
+   export default TodoList;
