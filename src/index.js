bbb3b7ad6823ecb9cba05cf686a948be8ed70682
Shushan Mirzoyan <shushanmirzoyan1@gmail.com>
2023-08-27 19:30:39 +0400

    new changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import Header from './components/Header'
 import Search from './components/Search'
 import TodoList from './components/TodoList'
-import UserList from './components/UserList';
+
+import UserListItem from './components/UserListItem';
 
 
 
@@ -12,6 +13,13 @@ import UserList from './components/UserList';
 
  const App = () =>{
   
+  const users = [
+    { name: 'John', age: 25, status: true },
+    { name: 'Bob', age: 30, status: false },
+    { name: 'Tom', age: 20, status: true },
+    { name: 'Sam', age: 28, status: false },
+    { name: 'Ann', age: 24, status: true },
+  ]
   
 
   return(
@@ -19,7 +27,15 @@ import UserList from './components/UserList';
      <Header/>
      <Search/>
      <TodoList/>
-     <UserList/>
+     
+     <ul>
+        {
+           users.map(({ name, age, status}) => {
+             return (<UserListItem name = {name} age = {age} status = {status}/>)
+           } )
+        }
+
+      </ul>
     </div>
   )
  }
