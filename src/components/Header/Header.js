ffe9852390ca14d2e9536063d2041427b1a38ed7
Shushan Mirzoyan <shushanmirzoyan1@gmail.com>
2023-08-31 15:42:39 +0400

    adding header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,13 @@ import './header.css'
 const Header = () => {
     return (
       <div className='header'>
-        <h1>Todo list</h1>
+        <h1 >Todo list </h1>
+          <span className='mark-up'>
+            <p>Done:12  Important:34</p>
+          
+          </span>
+        
+         
         
       </div>
     )
